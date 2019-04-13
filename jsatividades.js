6a92985234614cf160d709cffaa392436ab5faae
Natanzin <natan.silva.judo@gmail.com>
2019-04-13 11:02:54 -0300

    commit inicial

---
+++ jsatividades.js
@@ -0,0 +1,142 @@
+//lista de atividades 1
+function mudaCor1_1(cor){
+    var element = document.getElementById("lista1_1");
+    element.style.color = cor;
+}
+function mudaCor1_2(cor){
+    var element = document.getElementById("lista1_2");
+    element.style.color = cor;
+}
+function mudaCor1_3(cor){
+    var element = document.getElementById("lista1_3");
+    element.style.color = cor;
+}
+function mudaCor1_4(cor){
+    var element = document.getElementById("lista1_4");
+    element.style.color = cor;
+}
+function mudaCor1_5(cor){
+    var element = document.getElementById("lista1_5");
+    element.style.color = cor;
+}
+function mudaCor1_6(cor){
+    var element = document.getElementById("lista1_6");
+    element.style.color = cor;
+}
+//lista de atividades 2
+function mudaCor2_1(cor){
+    var element = document.getElementById("lista2_1");
+    element.style.color = cor;
+}
+function mudaCor2_2(cor){
+    var element = document.getElementById("lista2_2");
+    element.style.color = cor;
+}
+function mudaCor2_3(cor){
+    var element = document.getElementById("lista2_3");
+    element.style.color = cor;
+}
+function mudaCor2_4(cor){
+    var element = document.getElementById("lista2_4");
+    element.style.color = cor;
+}
+function mudaCor2_5(cor){
+    var element = document.getElementById("lista2_5");
+    element.style.color = cor;
+}
+//lista de atividades 3
+function mudaCor3_1(cor){
+    var element = document.getElementById("lista3_1");
+    element.style.color = cor;
+}
+function mudaCor3_2(cor){
+    var element = document.getElementById("lista3_2");
+    element.style.color = cor;
+}
+function mudaCor3_3(cor){
+    var element = document.getElementById("lista3_3");
+    element.style.color = cor;
+}
+function mudaCor3_4(cor){
+    var element = document.getElementById("lista3_4");
+    element.style.color = cor;
+}
+function mudaCor3_5(cor){
+    var element = document.getElementById("lista3_5");
+    element.style.color = cor;
+}
+function mudaCor3_6(cor){
+    var element = document.getElementById("lista3_6");
+    element.style.color = cor;
+}
+function mudaCor3_7(cor){
+    var element = document.getElementById("lista3_7");
+    element.style.color = cor;
+}
+//lista de atividades 4
+function mudaCor4_1(cor){
+    var element = document.getElementById("lista4_1");
+    element.style.color = cor;
+}
+function mudaCor4_2(cor){
+    var element = document.getElementById("lista4_2");
+    element.style.color = cor;
+}
+function mudaCor4_3(cor){
+    var element = document.getElementById("lista4_3");
+    element.style.color = cor;
+}
+function mudaCor4_4(cor){
+    var element = document.getElementById("lista4_4");
+    element.style.color = cor;
+}
+function mudaCor4_5(cor){
+    var element = document.getElementById("lista4_5");
+    element.style.color = cor;
+}
+//lista de atividades 5
+function mudaCor5_1(cor){
+    var element = document.getElementById("lista5_1");
+    element.style.color = cor;
+}
+function mudaCor5_2(cor){
+    var element = document.getElementById("lista5_2");
+    element.style.color = cor;
+}
+function mudaCor5_3(cor){
+    var element = document.getElementById("lista5_3");
+    element.style.color = cor;
+}
+function mudaCor5_4(cor){
+    var element = document.getElementById("lista5_4");
+    element.style.color = cor;
+}
+function mudaCor5_5(cor){
+    var element = document.getElementById("lista5_5");
+    element.style.color = cor;
+}
+function mudaCor5_6(cor){
+    var element = document.getElementById("lista5_6");
+    element.style.color = cor;
+}
+//lista de atividades 6
+function mudaCor6_1(cor){
+    var element = document.getElementById("lista6_1");
+    element.style.color = cor;
+}
+function mudaCor6_2(cor){
+    var element = document.getElementById("lista6_2");
+    element.style.color = cor;
+}
+function mudaCor6_3(cor){
+    var element = document.getElementById("lista6_3");
+    element.style.color = cor;
+}
+function mudaCor6_4(cor){
+    var element = document.getElementById("lista6_4");
+    element.style.color = cor;
+}
+function mudaCor6_5(cor){
+    var element = document.getElementById("lista6_5");
+    element.style.color = cor;
+}
